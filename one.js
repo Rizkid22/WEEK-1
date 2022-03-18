69008af6148cbacdcea32ef6cd867f3a0b5b3055
lawal ridwan <lawalridwanullahi20@gmail.com>
2022-03-18 13:35:08 -0700

    Your branch is up to date with 'origin/main'. 	new file:   one.html initial commit

---
+++ one.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+console.log('halo dunia');
+console.log('ciao mondo');
+console.log('hola mundo');
+console.log('ola mundo');
+console.log('ahlan aleaalamia');
+console.log('hallo welt');
+console.log('namaste shabdkosh');
+console.log(' habari dunia');
+console.log('vanakkam ulakam');
